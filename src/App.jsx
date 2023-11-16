c10f7bd529b08f0228eda9bb221730074aa8311e
IbrahimGazi_2022~ <coder.ibrahimgazi@gmail.com>
2023-11-17 05:32:06 +0600

    Adding Scrolling Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>About</section>
+      <section>Parallax</section>
+      <section>Skill</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+};
 
-export default App
+export default App;
